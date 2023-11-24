854f2b3f32aa4d4e2a658f6ddccfffc053fc2149
topurayhan007 <topurayhan007@gmail.com>
2023-11-24 21:31:57 +0600

    all setup done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
 const app: Application = express();
 
@@ -7,8 +7,8 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
-import app from "./app";
-import config from "./app/config";
-import mongoose from "mongoose";
+import app from './app';
+import config from './app/config';
+import mongoose from 'mongoose';
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`);
+      console.log(`App is listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
