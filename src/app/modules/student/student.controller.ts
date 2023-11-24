21cac333dee9e1ea1fd1ec50aefa6250ae3ce76b
topurayhan007 <topurayhan007@gmail.com>
2023-11-25 01:41:34 +0600

    some basic query

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -19,6 +19,37 @@ const createStudent = async (req: Request, res: Response) => {
   }
 };
 
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getAllStudentsFromDB();
+
+    res.status(200).json({
+      status: true,
+      message: 'Students retrieved successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+
+    res.status(200).json({
+      status: true,
+      message: 'Student is retrieved successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const StudentControllers = {
   createStudent,
+  getAllStudents,
+  getSingleStudent,
 };
--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -6,4 +6,8 @@ const router = express.Router();
 // will call controller function
 router.post('/create-student', StudentControllers.createStudent);
 
+router.get('/', StudentControllers.getAllStudents);
+
+router.get('/:studentId', StudentControllers.getSingleStudent);
+
 export const StudentRoutes = router;
